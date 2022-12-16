10d90f9701cbd94a02a0d29f07fd7e4883ebde40
EmsTravel <emacondulet@yahoo.com>
2022-12-16 12:33:19 +0100

    Middleware auth cree

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express'); // call express
 const router = express.Router(); // call router
 
+const auth = require('../middleware/auth');
+
 const userCtrl = require('../controllers/user'); // import controller
 
 router.post('/signup', userCtrl.signup);
